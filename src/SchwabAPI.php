f7c478fcbecd0ef2074af8322273551c60e2d414
Michael Drennen <michaeldrennen74@gmail.com>
2024-10-19 12:11:58 -0600

    Commit.

--- src/SchwabAPI.php
+++ src/SchwabAPI.php
@@ -4,32 +4,107 @@ namespace MichaelDrennen\SchwabAPI;
 
 use GuzzleHttp\Client;
 
+
+/**
+ * Customer Service Number for when I get locked out
+ * 800-780-2755
+ */
 class SchwabAPI {
 
 
     use APIClientTrait;
 
 
-    private string $apiKey;
-    private string $apiSecret;
-    private string $apiCallbackUrl;
+    protected string $apiKey;
 
-    protected bool $debug = false;
+
+    protected string $apiSecret;
+
+
+    protected string $apiCallbackUrlAuthenticate;
+
+    protected string $apiCallbackUrlToken;
 
     /**
-     * @var \GuzzleHttp\Client The Guzzle HTTP client so I can make requests to the Schwab API.
+     * @var string Returned from https://api.schwabapi.com/v1/oauth/authorize?
+     * @url https://developer.schwab.com/products/trader-api--individual/details/documentation/Retail%20Trader%20API%20Production
+     */
+    protected ?string $code = null;
+
+    protected ?string $token = null;
+
+
+    /**
+     * @var string Also returned from https://api.schwabapi.com/v1/oauth/authorize?
+     * @url https://developer.schwab.com/products/trader-api--individual/details/documentation/Retail%20Trader%20API%20Production
+     */
+    protected string $session;
+
+
+    protected bool $debug = FALSE;
+
+
+    /**
+     * @var \GuzzleHttp\Client The Guzzle HTTP client, so I can make requests to the Schwab API.
      */
     protected Client $client;
 
-    public function __construct( string $apiKey, string $apiSecret, string $apiCallbackUrl, bool $debug = FALSE ) {
 
-        $this->apiKey         = $apiKey;
-        $this->apiSecret      = $apiSecret;
-        $this->apiCallbackUrl = $apiCallbackUrl;
-        $this->debug          = $debug;
+    /**
+     * @param string      $apiKey
+     * @param string      $apiSecret
+     * @param string      $apiCallbackUrlAuthenticate
+     * @param string      $apiCallbackUrlToken
+     * @param string|null $authenticationCode
+     * @param string|null $token
+     * @param bool        $debug
+     */
+    public function __construct( string $apiKey,
+                                 string $apiSecret,
+                                 string $apiCallbackUrlAuthenticate,
+                                 string $apiCallbackUrlToken,
+                                 string $authenticationCode = NULL,
+                                 string $token = NULL,
+                                 bool   $debug = FALSE ) {
+
+        $this->apiKey                     = $apiKey;
+        $this->apiSecret                  = $apiSecret;
+        $this->apiCallbackUrlAuthenticate = $apiCallbackUrlAuthenticate;
+        $this->apiCallbackUrlToken        = $apiCallbackUrlToken;
+        $this->code                       = $authenticationCode;
+        $this->token                      = $token;
+        $this->debug                      = $debug;
 
 
-        $this->client = $this->createGuzzleClient();
+        $this->client = $this->createGuzzleClient( $token, $this->debug );
+    }
+
+    public function getAuthorizeUrl(): string {
+        return 'https://api.schwabapi.com/v1/oauth/authorize?client_id=' . $this->apiKey . '&redirect_uri=' . urlencode( $this->apiCallbackUrlAuthenticate );
+    }
+
+
+    /**
+     * Setter for the 'code' returned from the Schwab authorization request
+     *
+     * @param string $code
+     *
+     * @return void
+     */
+    public function setCode( string $code ): void {
+        $this->code = $code;
+    }
+
+
+    /**
+     * Setter for the 'session' returned from the Schwab authorization request
+     *
+     * @param string $session
+     *
+     * @return void
+     */
+    public function setSession( string $session ): void {
+        $this->session = $session;
     }
 
 
@@ -38,21 +113,59 @@ class SchwabAPI {
      * @return void
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function authenticate() {
-        $uri      = 'https://api.schwabapi.com/v1/oauth/authorize?client_id=' . $this->apiKey . '&redirect_uri=' . $this->apiCallbackUrl;
+    public function requestAuthenticate() {
+        $uri = 'https://api.schwabapi.com/v1/oauth/authorize?client_id=' . $this->apiKey . '&redirect_uri=' . urlencode( $this->apiCallbackUrlAuthenticate );
 
+        var_dump( $uri );
+        //die('asdf');
         try {
             $response = $this->client->get( $uri );
             print_r( $response );
-        }
-        catch (\GuzzleHttp\Exception\ClientException $e) {
-            $response = $e->getResponse();
+        } catch ( \GuzzleHttp\Exception\ClientException $e ) {
+            $response             = $e->getResponse();
             $responseBodyAsString = $response->getBody()->getContents();
             print_r( $responseBodyAsString );
         }
+    }
 
 
+    /**
+     * curl -X POST https://api.schwabapi.com/v1/oauth/token \
+     * -H 'Authorization: Basic {BASE64_ENCODED_Client_ID:Client_Secret} \
+     * -H 'Content-Type: application/x-www-form-urlencoded' \
+     * -d 'grant_type=authorization_code&code={AUTHORIZATION_CODE_VALUE}&redirect_uri=https://example_url.com/callback_example'
+     *
+     * @return void
+     */
+    public function requestToken(): string {
+        try {
 
+            $options = [
+                'headers' => [
+                    'Authorization' => "Basic " . base64_encode( $this->apiSecret ),
+                    'Content-Type'  => 'application/x-www-form-urlencoded',
+                ],
+                //'body'    => 'grant_type=authorization_code&code=' . $this->code . '&redirect_uri=' . urlencode( $this->apiCallbackUrlToken ),
+                'debug' => true,
+                'form_params' => [
+                    'grant_type' => 'authorization_code',
+                    'code' => urlencode($this->code),
+                    'redirect_uri' => urlencode( $this->apiCallbackUrlToken )
+                ]
+            ];
+
+            //dd($options);
+
+
+            $response = $this->client->post( 'https://api.schwabapi.com/v1/oauth/token', $options  );
+            print_r( $response );
+        } catch ( \GuzzleHttp\Exception\ClientException $e ) {
+            echo "\nThis is the error response from the guzzle client:\n";
+            $response             = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+            print_r( $responseBodyAsString );
+        }
+        return "test token";
     }
 
 
--- tests/CharlesSchwabApiTest.php
+++ tests/CharlesSchwabApiTest.php
@@ -3,27 +3,58 @@
 namespace MichaelDrennen\SchwabAPI\Tests;
 
 
+use GuzzleHttp\Client;
 use MichaelDrennen\SchwabAPI\SchwabAPI;
 use PHPUnit\Framework\TestCase;
 
+
+if ( !function_exists( 'dd' ) ) {
+    function dd() {
+        echo '<pre>';
+        array_map( function ( $x ) {
+            var_dump( $x );
+        }, func_get_args() );
+        die;
+    }
+}
+
 class CharlesSchwabApiTest extends TestCase {
 
+    protected string $code;
+    protected string $session;
+
+    protected SchwabAPI $api;
+
+    protected function setUp(): void {
+
+        $this->code                 = $_ENV[ 'CODE' ];
+        $this->session              = $_ENV[ 'SESSION' ];
+        $apiKey                     = $_ENV[ 'SCHWAB_API_KEY' ];
+        $apiSecret                  = $_ENV[ 'SCHWAB_API_SECRET' ];
+        $apiCallbackUrlAuthenticate = $_ENV[ 'SCHWAB_AUTHENTICATE_CALLBACK_URL' ];
+        $apiCallbackUrlToken        = $_ENV[ 'SCHWAB_TOKEN_CALLBACK_URL' ];
+        $debug                      = TRUE;
+
+
+        $this->api = new SchwabAPI( $apiKey,
+                                    $apiSecret,
+                                    $apiCallbackUrlAuthenticate,
+                                    $apiCallbackUrlToken,
+                                    $this->code,
+                                    null,
+                                    $debug );
+
+    }
 
     /**
      * @test
      */
     public function testConstructorShouldCreateApiObject() {
-        $apiKey         = $_ENV[ 'SCHWAB_API_KEY' ];
-        $apiSecret      = $_ENV[ 'SCHWAB_API_SECRET' ];
-        $apiCallbackUrl = $_ENV[ 'SCHWAB_CALLBACK_URL' ];
-        $debug          = FALSE;
-        $api            = new SchwabAPI( $apiKey, $apiSecret, $apiCallbackUrl, $debug );
 
+        $this->assertInstanceOf( SchwabAPI::class, $this->api );
+    }
 
-
-
-
-        $api->authenticate();
-        $this->assertInstanceOf( SchwabAPI::class, $api );
+    public function testGetTokenShouldGetToken() {
+        $token = $this->api->requestToken();
     }
 }
