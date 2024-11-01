aa0fc6552161a23e13d53f124c2c97501d293f8b
Michael Drennen <michaeldrennen74@gmail.com>
2024-11-01 13:02:30 -0600

    Removed superfluous code and cleaned up the documentation.

--- src/SchwabAPI.php
+++ src/SchwabAPI.php
@@ -118,9 +118,12 @@ class SchwabAPI {
         $this->client = $this->createGuzzleClient( $token, $this->debug );
     }
 
+
     /**
      * The first step in authorizing your app to access your Schwab trading account, is to direct
      * your user to the URL returned by this method.
+     *
+     * @url https://developer.schwab.com/products/trader-api--individual/details/documentation/Retail%20Trader%20API%20Production
      *
      * @return string
      */
@@ -167,6 +170,11 @@ class SchwabAPI {
      * This method will get called from the CALLBACK URL of yours, after the
      * Schwab site directs back to your CALLBACK URL after authorization.
      *
+     *  curl -X POST https://api.schwabapi.com/v1/oauth/token \
+     *  -H 'Authorization: Basic {BASE64_ENCODED_Client_ID:Client_Secret} \
+     *  -H 'Content-Type: application/x-www-form-urlencoded' \
+     *  -d 'grant_type=authorization_code&code={AUTHORIZATION_CODE_VALUE}&redirect_uri=https://example_url.com/callback_example'
+     *
      * @param bool $doRefreshToken
      *
      * @return void
@@ -201,7 +209,6 @@ class SchwabAPI {
             $response = $this->client->post( 'https://api.schwabapi.com/v1/oauth/token', $options );
             $json     = $response->getBody()->getContents();
 
-
             /**
              * @var array $data A php array of the data sent back from the ...token URL we just posted to.
              *
@@ -234,9 +241,6 @@ class SchwabAPI {
     }
 
 
-
-
-
     /**
      * A simple getter for the accessToken.
      *
@@ -255,72 +259,4 @@ class SchwabAPI {
     public function getRefreshToken(): string {
         return $this->refreshToken;
     }
-
-
-
-
-
-
-
-    /**
-     * @url https://developer.schwab.com/products/trader-api--individual/details/documentation/Retail%20Trader%20API%20Production
-     * @return void
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    //public function requestAuthenticate() {
-    //    $uri = $this->getAuthorizeUrl();
-    //
-    //    var_dump( $uri );
-    //    die( 'asdf' );
-    //    try {
-    //        $response = $this->client->get( $uri );
-    //        print_r( $response );
-    //    } catch ( \GuzzleHttp\Exception\ClientException $e ) {
-    //        $response             = $e->getResponse();
-    //        $responseBodyAsString = $response->getBody()->getContents();
-    //        print_r( $responseBodyAsString );
-    //    }
-    //}
-
-
-    /**
-     * curl -X POST https://api.schwabapi.com/v1/oauth/token \
-     * -H 'Authorization: Basic {BASE64_ENCODED_Client_ID:Client_Secret} \
-     * -H 'Content-Type: application/x-www-form-urlencoded' \
-     * -d 'grant_type=authorization_code&code={AUTHORIZATION_CODE_VALUE}&redirect_uri=https://example_url.com/callback_example'
-     *
-     * @return string
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    //public function requestToken(): string {
-    //    try {
-    //
-    //        $options = [
-    //            'headers'     => [
-    //                'Authorization' => "Basic " . base64_encode( $this->apiSecret ),
-    //                'Content-Type'  => 'application/x-www-form-urlencoded',
-    //            ],
-    //            'debug'       => TRUE,
-    //            'form_params' => [
-    //                'grant_type'   => 'authorization_code',
-    //                'code'         => urlencode( $this->code ),
-    //                'redirect_uri' => urlencode( $this->apiCallbackUrl ),
-    //            ],
-    //        ];
-    //
-    //        //dd($options);
-    //
-    //
-    //        $response = $this->client->post( 'https://api.schwabapi.com/v1/oauth/token', $options );
-    //        print_r( $response );
-    //    } catch ( \GuzzleHttp\Exception\ClientException $e ) {
-    //        echo "\nThis is the error response from the guzzle client:\n";
-    //        $response             = $e->getResponse();
-    //        $responseBodyAsString = $response->getBody()->getContents();
-    //        print_r( $responseBodyAsString );
-    //    }
-    //    return "test token";
-    //}
-
-
 }
