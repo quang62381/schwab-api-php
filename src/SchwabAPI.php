b8388428e33ff1eb0a4ba71d920d4d05ef1e759b
Michael Drennen <michaeldrennen74@gmail.com>
2024-11-02 09:10:59 -0600

    Creating some API request methods.

--- src/SchwabAPI.php
+++ src/SchwabAPI.php
@@ -2,6 +2,7 @@
 
 namespace MichaelDrennen\SchwabAPI;
 
+use Carbon\Carbon;
 use GuzzleHttp\Client;
 use MichaelDrennen\SchwabAPI\Exceptions\RequestException;
 
@@ -294,11 +295,19 @@ class SchwabAPI {
     }
 
 
-    // REQUESTS
+    /**
+     * REQUESTS
+     */
+
+
+    /**
+     * ACCOUNTS
+     */
 
     /**
      * print_r() of $data
      * Array ( [0] => Array ( [accountNumber] => 27834695236 [hashValue] => 397465203847059238764059762304 ) [1] => Array ( [accountNumber] => 08347502745 [hashValue] => A34502983740527304857203947580A535A67529CC ) )
+     * The 'hashValue' is the 'encrypted' accountNumber that is used in all other REQUESTS to the Schwab API system.
      *
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -309,21 +318,98 @@ class SchwabAPI {
 
 
     /**
+     * All the linked account information for the user logged in.
+     * The balances on these accounts are displayed by default, however
+     * the positions on these accounts will be displayed based on the "positions" flag.
+     *
+     * @param bool $positions Setting to TRUE adds a query parameter to the endpoint that tells Schwab to return POSITION data as well.
+     *
+     * @return array
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function accounts(): array {
+    public function accounts( bool $positions = FALSE ): array {
         $suffix = '/accounts';
+        if ( $positions ):
+            $suffix .= '?fields=positions';
+        endif;
         return $this->_request( $suffix );
     }
 
     /**
+     * @param string $hashValueOfAccountNumber This hash value is returned by the '/accounts/accountNumbers' endpoint. Ex: E49D5746FD010E582E364C28E9D6A763D972C3A0C0C90170878260D0A6C65453
+     * @param array  $fields                   In the Schwab docs, the only 'field' mentioned is 'positions' like in the accounts() method above. However, they elude to there being more.
+     *
+     * @return array
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function account( string $accountNumber): array {
-        $suffix = '/accounts/' . $accountNumber;
+    public function account( string $hashValueOfAccountNumber, array $fields = [] ): array {
+        $suffix = '/accounts/' . $hashValueOfAccountNumber;
+
+        if ( $fields ):
+            $suffix .= '?' . http_build_query( $fields );
+        endif;
+
         return $this->_request( $suffix );
     }
 
 
+    /**
+     * ORDERS
+     */
+
+    /**
+     * All orders for a specific account. Orders retrieved can be filtered based on input parameters below. Maximum date range is 1 year.
+     * @param string              $hashValueOfAccountNumber
+     * @param int|NULL            $maxResults The max number of orders to retrieve. Default is 3000.
+     * @param \Carbon\Carbon|NULL $fromTime Specifies that no orders entered before this time should be returned.
+     * @param \Carbon\Carbon|NULL $toTime Specifies that no orders entered after this time should be returned.
+     * @param string|NULL         $status Specifies that only orders of this status should be returned. Valid values can be found in the method body below.
+     *
+     * @return array
+     * @throws \Exception
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function orders( string $hashValueOfAccountNumber,
+                            int    $maxResults = NULL,
+                            Carbon $fromTime = NULL,
+                            Carbon $toTime = NULL,
+                            string $status = NULL ): array {
+        $suffix = '/accounts/' . $hashValueOfAccountNumber . '/orders';
+
+        $validStatuses = [
+            'AWAITING_PARENT_ORDER', 'AWAITING_CONDITION', 'AWAITING_STOP_CONDITION', 'AWAITING_MANUAL_REVIEW', 'ACCEPTED', 'AWAITING_UR_OUT', 'PENDING_ACTIVATION', 'QUEUED', 'WORKING', 'REJECTED', 'PENDING_CANCEL', 'CANCELED', 'PENDING_REPLACE', 'REPLACED', 'FILLED', 'EXPIRED', 'NEW', 'AWAITING_RELEASE_TIME', 'PENDING_ACKNOWLEDGEMENT', 'PENDING_RECALL', 'UNKNOWN',
+        ];
+
+        // This Exception is just to help Developers keep from getting confused or wasting time.
+        if ( $fromTime xor $toTime ):
+            throw new \Exception( "If you set fromTime, you are required to set toTime as well." );
+        endif;
+
+        $queryParameters = [];
+
+        if ( $maxResults ):
+            $queryParameters[ 'maxResults' ] = $maxResults;
+        endif;
+
+        if ( $fromTime && $toTime ):
+            $queryParameters[ 'fromTime' ] = $fromTime->toIso8601String();
+            $queryParameters[ 'toTime' ]   = $fromTime->toIso8601String();
+        endif;
+
+        if ( $status ):
+            $status = strtoupper( $status );
+            if ( !in_array( $status, $validStatuses ) ):
+                throw new \Exception( "Invalid status. Your input of '" . $status . "' is not in the array of validStatuses." );
+            endif;
+
+            $queryParameters[ 'status' ] = $status;
+        endif;
+
+        if ( $queryParameters ):
+            $suffix .= '?' . http_build_query( $queryParameters );
+        endif;
+
+        return $this->_request( $suffix );
+    }
 
 }
